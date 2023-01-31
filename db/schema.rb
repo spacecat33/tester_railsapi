7171e99fa321cd97d338a8b7f9431077553ebec4
Janan Akkad <jananakkad@hotmail.co.uk>
2023-01-31 12:17:31 +0000

    added event model, controller and db table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_31_115034) do
+ActiveRecord::Schema.define(version: 2023_01_31_121114) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
