3ffb6a654e0244c08d9350061fa7894f6dc91948
Janan Akkad <jananakkad@hotmail.co.uk>
2023-04-06 15:10:02 +0100

    added event listener and prevented refresh of page

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,6 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-const form = document.querySelector('form')
-console.log(form)
+const form = document.querySelector('form');
+console.log(form);
 
+form.addEventListener("submit", (event) => {
+        event.preventDefault();   //this prevents the default refresh behaviour and allows the input to be persisted
+        console.log(event);
+        const input = event.target[0];
+        const comment = input.value;
+        console.log(comment);
+    })
 })
