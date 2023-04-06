217bf814795ccaa640af39ce34266d652c3b6fe3
Janan Akkad <jananakkad@hotmail.co.uk>
2023-04-06 15:17:00 +0100

    updated event listener to be more dry

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -3,11 +3,14 @@ document.addEventListener("DOMContentLoaded", function() {
 const form = document.querySelector('form');
 console.log(form);
 
-form.addEventListener("submit", (event) => {
+form.addEventListener("submit", handleSubmit)
+})
+
+function handleSubmit(event) {
         event.preventDefault();   //this prevents the default refresh behaviour and allows the input to be persisted
         console.log(event);
-        const input = event.target[0];
-        const comment = input.value;
+        // const input = event.target[0];
+        // const comment = input.value;    //OR INSTEAD OF THIS LINE AND LINE ABOVE, CAN PUT:
+        const comment = document.getElementById("new-comment").value
         console.log(comment);
-    })
-})
+    }
